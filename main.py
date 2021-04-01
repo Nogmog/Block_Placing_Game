ded56ce1e9a2b2369e0b3f09b68a6aa16459918c
Steven PC <iexplodeh@hotmail.com>
2021-04-01 22:09:17 +0100

    Attempting to put image on screen (not fitting)

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ O_img = pygame.image.load(os.path.join("Assets", "O block.png"))
 S_img = pygame.image.load(os.path.join("Assets", "S block.png"))
 Z_img = pygame.image.load(os.path.join("Assets", "Z block.png"))
 T_img = pygame.image.load(os.path.join("Assets", "T block.png"))
-
+block_images = [I_img, J_img, L_img, O_img, S_img, Z_img, T_img]
+resized_blocks = []
 #"TEMPORARY" COLOURS
 YELLOW = (255, 255, 0)
 DARK_GREY = (64, 64, 64)
@@ -35,6 +36,11 @@ block_per_grid = (HEIGHT - HEIGHT*0.1) // 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BLOCK PLACING GAME")
 pygame.display.set_icon(PROGRAMICON)
+
+def resize_images(size):
+    for i in range(len(block_images) - 1):
+        block_images[i] = pygame.transform.scale(block_images[i], (size, size))
+        #resized_blocks.append(pygame.transform.scale(block_images[i], (size, size)))
 
 def blank_board():
     board = []
@@ -76,15 +82,26 @@ def create_grid(game_state):
     
 def show_queue(queue):
     total_width = block_per_grid * 3
-    total_height = block_per_grid * 8
+    total_height = block_per_grid * 10 #queue_length + block_per_grid
     starting_x, starting_y = (WIDTH / 2) + block_per_grid * 6, (HEIGHT / 2) - ( (block_per_grid * 20) / 2)
 
     background_area = pygame.Rect(starting_x, starting_y, total_width, total_height) 
     pygame.draw.rect(WINDOW, YELLOW, background_area)
+
+    size = int((total_height - block_per_grid) // 9)
+    resize_images(size)
+    
+    centralise_x = int(total_width // 2 - size // 2) + starting_x
+
+    for i in range(len(queue) - 1):
+        height = size * (i + 1)
+        if queue[i] == "S":
+            print(i,": S block")
+            WINDOW.blit(S_img, (centralise_x, height))
     
 def update_queue(queue):
     global bag_queue_14
-    while len(queue) < 7:
+    while len(queue) < queue_length:
         if len(bag_queue_14) == 0:
             bag_queue_14 = ["S", "S", "Z", "Z", "I", "I", "O", "O", "J", "J", "L", "L", "T", "T"]
 
